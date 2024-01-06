3a2e5717b698c16acb4da8059496e61873cc3baa
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-01-06 21:34:35 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import styles from './App.module.css';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
-      <div className={styles.App}>Hello</div>
+      <div className={styles.App}>
+        <Navbar />
+      </div>
     </>
   );
 }
