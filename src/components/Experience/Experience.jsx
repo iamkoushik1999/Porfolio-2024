974a51ab4c02e3acf2e5f3369ad9eee2011f4b57
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-01-12 21:44:01 +0530

    Experience Section

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,48 @@
+// import React from 'react'
+
+import skills from '../../data/skills.json';
+import history from '../../data/history.json';
+import { getImageUrl } from '../../utils';
+
+export const Experience = () => {
+  return (
+    <section id='experiance'>
+      <h2>Experience</h2>
+      <div className=''>
+        <div className=''>
+          {skills.map((skill, id) => {
+            return (
+              <div key={id}>
+                <div>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            );
+          })}
+        </div>
+        <ul>
+          {history.map((historyItem, id) => {
+            return (
+              <li key={id}>
+                <img
+                  src={getImageUrl(historyItem.imageSrc)}
+                  alt={`${historyItem.organisation} Logo`}
+                />
+                <div>
+                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                      return <li key={id}>{experience}</li>;
+                    })}
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
