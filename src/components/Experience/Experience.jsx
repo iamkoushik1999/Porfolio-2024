d56dbab018dddff4fd507fe976f344ad774f7045
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-01-13 21:58:28 +0530

    Experience Section Css

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,35 +1,41 @@
 // import React from 'react'
 
+import styles from './Experience.module.css';
+// Data
 import skills from '../../data/skills.json';
 import history from '../../data/history.json';
 import { getImageUrl } from '../../utils';
 
 export const Experience = () => {
   return (
-    <section id='experiance'>
-      <h2>Experience</h2>
-      <div className=''>
-        <div className=''>
+    <section className={styles.container} id='experiance'>
+      <h2 className={styles.title}>Experience</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
           {skills.map((skill, id) => {
             return (
-              <div key={id}>
+              <div className={styles.skill} key={id}>
                 <div>
-                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                  <img
+                    className={styles.skillImageContainer}
+                    src={getImageUrl(skill.imageSrc)}
+                    alt={skill.title}
+                  />
                 </div>
                 <p>{skill.title}</p>
               </div>
             );
           })}
         </div>
-        <ul>
+        <ul className={styles.history}>
           {history.map((historyItem, id) => {
             return (
-              <li key={id}>
+              <li className={styles.historyItem} key={id}>
                 <img
                   src={getImageUrl(historyItem.imageSrc)}
                   alt={`${historyItem.organisation} Logo`}
                 />
-                <div>
+                <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                   <ul>
