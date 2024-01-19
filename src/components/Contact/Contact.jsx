bc49c07248b9e38e3e8b5c1a1a44aaa0b0979459
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-01-19 23:02:10 +0530

    Contact Section Css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,28 +1,29 @@
 // import React from 'react'
 
+import styles from './Contact.module.css';
 import { getImageUrl } from '../../utils';
 
 export const Contact = () => {
   return (
-    <footer id='contact'>
-      <div>
+    <footer id='contact' className={styles.container}>
+      <div className={styles.text}>
         <h2>Contact</h2>
         <p>Feel free to reach out</p>
       </div>
-      <ul>
-        <li>
+      <ul className={styles.links}>
+        <li className={styles.link}>
           <img src={getImageUrl('contact/emailIcon.png')} alt='Icon' />
           <a href='mailto:iamkoushik1999@gmail.com'>Email</a>
         </li>
-        <li>
+        <li className={styles.link}>
           <img src={getImageUrl('contact/linkedinIcon.png')} alt='Icon' />
           <a href='https://www.linkedin.com/in/koushikdutta/'>Linked In</a>
         </li>
-        <li>
+        <li className={styles.link}>
           <img src={getImageUrl('contact/emailIcon.png')} alt='Icon' />
           <a href='https://github.com/iamkoushik1999'>Github</a>
         </li>
-        <li>
+        <li className={styles.link}>
           <img src={getImageUrl('contact/linkIcon.png')} alt='Icon' />
           <a href='https://linktr.ee/iamkoushik1999'>All Links</a>
         </li>
